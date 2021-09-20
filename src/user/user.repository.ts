27cd92066716c5998668c4c36c2d497ebf603133
ryuji <ryu.seven.h7420@gmail.com>
2021-09-20 17:05:42 +0900

    ユーザ登録機能実装

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,6 +2,10 @@ import { EntityRepository, Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 import { SignUpCredentialsDto } from './dto/signup-credentials.dto';
 import { User } from './user.entity';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,13 +13,22 @@ export class UserRepository extends Repository<User> {
   async signUp(signUpCredentialsDto: SignUpCredentialsDto): Promise<any> {
     const { userName, email, password } = signUpCredentialsDto;
 
-    const salt = await bcrypt.genSalt();
+    const user = this.create();
+    user.userName = userName;
+    user.email = email;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
-    return {
-      userName,
-      email,
-      password: await this.hashPassword(password, salt),
-    };
+    try {
+      await user.save();
+      return { userName, email };
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('This email already registerd');
+      } else {
+        throw new InternalServerErrorException(error);
+      }
+    }
   }
 
   private async hashPassword(password: string, salt: string): Promise<string> {
